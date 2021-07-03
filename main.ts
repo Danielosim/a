ea0499328442ad4281dee8a32672f428eac2036a
Danielosim <86317406+Danielosim@users.noreply.github.com>
2021-07-03 11:18:33 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+OLED.init(128, 64)
+OLED.writeStringNewLine("")
+OLED.writeNumNewLine(0)
+OLED.writeString("")
+OLED.writeNum(0)
+OLED.newLine()
+OLED.clear()
+OLED.drawLoading(0)
+OLED.drawLine(
+0,
+0,
+20,
+20
+)
+OLED.drawRectangle(
+0,
+0,
+20,
+20
+)
 basic.forever(function () {
 	
 })
