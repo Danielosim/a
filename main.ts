cdb3e58e6b21ad47a168ab0ca4110f2aa3e2fb8c
Danielosim <86317406+Danielosim@users.noreply.github.com>
2021-07-03 17:21:14 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
-basic.showLeds(`
-    # # # # #
-    . . # . .
-    . . # . .
-    . . # . .
-    . . # # .
-    `)
 basic.forever(function () {
-	
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("")
+    OLED.writeNumNewLine(0)
+    OLED.writeString("")
+    OLED.writeNum(0)
+    OLED.newLine()
+    OLED.clear()
+    OLED.drawLoading(0)
+    OLED.drawLine(
+    0,
+    0,
+    20,
+    20
+    )
 })
