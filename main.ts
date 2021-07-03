cda57d92145f17102ff15977fcd110f39b43b458
Danielosim <86317406+Danielosim@users.noreply.github.com>
2021-07-03 11:26:02 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,10 @@
-OLED.init(128, 64)
-OLED.writeStringNewLine("")
-OLED.writeNumNewLine(0)
-OLED.writeString("")
-OLED.writeNum(0)
-OLED.newLine()
-OLED.clear()
-OLED.drawLoading(0)
-OLED.drawLine(
-0,
-0,
-20,
-20
-)
-OLED.drawRectangle(
-0,
-0,
-20,
-20
-)
+basic.showLeds(`
+    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
 basic.forever(function () {
 	
 })
